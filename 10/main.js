df9baab49936b13c4d35bf3b650ca59ab303b8c9
luzyla <luzyla@gmail.com>
2020-08-16 05:11:56 -0300

    Ejercicio 10

--- 10/main.js
+++ 10/main.js
@@ -1,10 +1,16 @@
-const sumar = (numeroA, numeroB) => {
+const obtenerDatosDeCiudad = (nombre, población, país) => {
     
-    console.log(numeroA + numeroB);
+    return `La ciudad de ${nombre} tiene una población de ${población} habitantes y está ubicada en ${país}.`
 }
 
-sumar(10, 20)
-sumar(8, 8)
-sumar(38, 34)
+obtenerDatosDeCiudad('Buenos Aires', 2000000, 'Argentina')
+console.log(obtenerDatosCiudad('Buenos Aires', 2000000, 'Argentina'))
+
+obtenerDatosDeCiudad('St. Johns', 100000, 'Canada')
+console.log(obtenerDatosCiudad('St. Johns', 100,000, 'Canada'))
+
+obtenerDatosDeCiudad('Londres', 2500000, 'United Kingdom')
+console.log(obtenerDatosCiudad('Londres', 2500000, 'United Kingdom'))
+
 
 // ``
